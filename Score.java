2b0800d99442e71e407a1575970eaf076cdc51df
Daniel <d.barenholz@student.tue.nl>
2018-02-25 14:09:18 +0100

    Add score.java file.

---
+++ Score.java
@@ -0,0 +1,19 @@
+
+package learninGame;
+
+// Own packages
+
+public class Score {
+    /*
+     * Constructor
+     */
+    public Score() {
+
+    }
+
+    /*
+     * Prints the current score. Todo.
+     */
+    @Overrides
+    public void toString() {}
+}
