5e68ec1e90224239816f573b81d80819f9626b4c
Kaj0Wortel <36736012+Kaj0Wortel@users.noreply.github.com>
2018-03-08 23:57:26 +0100

    Added score screen template

---
+++ ScoreScreen.java
@@ -0,0 +1,24 @@
+
+package learningGame;
+
+
+// Own packages
+
+
+// Java pacakges
+import javax.swing.JPanel;
+
+/* 
+ * Gives a visual representation of the given score object.
+ */
+public class ScoreScreen extends JPanel {
+    public ScoreScreen(Score score) {
+        // todo
+        
+        createGUI();
+    }
+    
+    public void createGUI() {
+        // todo
+    }
+}
