f520237d8623432da40bb7e1f6063f425766bbed
Kaj0Wortel <36736012+Kaj0Wortel@users.noreply.github.com>
2018-03-08 16:27:33 +0100

    Added functions, added images.

--- Word.java
+++ Word.java
@@ -7,10 +7,13 @@ import learningGame.LearningGame;
 import learningGame.MiniGame;
 import learningGame.log.Log2;
 import learningGame.tools.BufferedReaderPlus;
+import learningGame.tools.LoadImages2;
 import learningGame.tools.MultiTool;
 
 
 // Java packages
+import java.awt.image.BufferedImage;
+
 import java.io.File;
 import java.io.IOException;
 
@@ -26,19 +29,26 @@ public class Word {
     // The MiniGame that is attatched to this word.
     final private Class<MiniGame> miniGameClass;
     
+    final private String wordImgLoc;
+    
     /* 
      * Constructor
      */
-    public Word(String[] words, String[] langs, Class<MiniGame> miniGameClass) throws IllegalArgumentException {
+    public Word(String[] words, String[] langs, Class<MiniGame> miniGameClass, String wordImgLoc)
+        throws IllegalArgumentException
+    {
+        if (wordImgLoc == null) 
+            throw new NullPointerException("The given image word location was null.");
         if (words == null) 
-            throw new IllegalArgumentException("The given word list is null.");
+            throw new NullPointerException("The given word list is null.");
         if (langs == null)
-            throw new IllegalArgumentException("The given language list is null.");
+            throw new NullPointerException("The given language list is null.");
         if (words.length != langs.length)
             throw new IllegalArgumentException("The length of the words and language lists are unequal: "
                                          + "words.length() = " + words.length + ", "
                                          + "language.length() = " + langs.length + ".");
         this.miniGameClass = miniGameClass;
+        this.wordImgLoc = wordImgLoc;
         
         // Put all words with their language in the hashtable.
         for (int i = 0; i < words.length; i++) {
@@ -54,8 +64,40 @@ public class Word {
         return wordTable.get(lang);
     }
     
+    /* 
+     * @return the MiniGame associated with this word.
+     */
     public Class<MiniGame> getMiniGameClass() {
         return miniGameClass;
+    }
+    
+    private Random rand = new Random();
+    /* 
+     * @return a random image that is associated with this word.
+     */
+    public BufferedImage getRandomImage() {
+        if (wordImgLoc == null || wordImgLoc.equals("")) return null;
+        
+        try {
+            BufferedImage[][] imgs = LoadImages2.ensureLoadedAndGetImage(wordImgLoc);
+            int x = rand.nextInt(imgs.length);
+            int y = rand.nextInt(imgs[x].length);
+            return imgs[x][y];
+            
+        } catch (IOException e) {
+            Log2.write(new Object[] {
+                ("Failed to load images of word: " + this.toString() + ". Error: "), e
+            }, Log2.ERROR);
+        }
+        
+        return null;
+    }
+    
+    /* 
+     * @return the word table of this word.
+     */
+    protected Hashtable getWordTable() {
+        return wordTable;
     }
     
     /* 
@@ -67,6 +109,16 @@ public class Word {
     }
     
     /* 
+     * Determines whether the two objects are equal or not.
+     */
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj) return true;
+        if (!(obj instanceof Word)) return false;
+        return this.getWordTable().equals(((Word) obj).getWordTable());
+    }
+    
+    /* 
      * Creates a MiniGame of a certain class, with a certain terminate action.
      * 
      * @param miniGame the class of the object that will be created.
@@ -75,7 +127,11 @@ public class Word {
      */
     public MiniGame createMiniGame(LearningGame lg, Runnable r) {
         if (miniGameClass == null) {
-            Log2.write("Cannot create an instance of a null class.", Log2.ERROR);
+            Log2.write(new String[] {
+                ("Class: " + this.getClass().getName()),
+                    ("Function: MiniGame createMiniGame(LearningGame, Runnable)"),
+                    ("Error: Cannot create a MiniGame from a null class.")
+            }, Log2.ERROR);
             return null;
         }
         
@@ -96,42 +152,75 @@ public class Word {
      * 
      * @param fileName the name of the file the words are read from.
      * @param miniGameDir the directory of the MiniGames.
+     * @param imgDir the directory where the images related to the word are stored.
      * @return an ArrayList with all words that were defined in 
      */
     @SuppressWarnings("unchecked") // For the cast from Object to Class
-    public static ArrayList<Word> createWordList(String fileName, String miniGameDir) {
+    public static ArrayList<Word> createWordList(String dataFileName, String miniGameDir, String imgDir) {
+        Log2.write(new String[] {
+            (" === Started creating word list === "),
+                ("Data file: \"" + dataFileName + "\""),
+                ("MiniGame dir: \"" + miniGameDir + "\""),
+                ("Image dir: \"" + imgDir + "\""),
+                ("")
+        }, Log2.INFO);
+        
         ArrayList<Word> words = new ArrayList<Word>();
         
-        try (BufferedReaderPlus brp = new BufferedReaderPlus(fileName, BufferedReaderPlus.JAVA_COMMENT, true)) {
-            // Retrieve the languages
+        try (BufferedReaderPlus brp = new BufferedReaderPlus(dataFileName, BufferedReaderPlus.JAVA_COMMENT, true)) {
+            // Obtain the languages
             String[] langs = MultiTool.listToArray(brp.readCSVLine(true), String.class, 1);
             if (langs == null) return null;
             
             ArrayList<String> cells;
             while ((cells = brp.readCSVLine(true)) != null) {
                 String className = "learningGame.miniGame." + cells.get(0);
+                
                 Class<MiniGame> miniGame = null;
                 
-                try {
-                    miniGame = (Class<MiniGame>) Class.forName(className);
-                    Log2.write("Succesfully loaded class: \"" + miniGame.getName() + "\".", Log2.INFO);
+                // If no data for the MiniGame class is available, then don't even bother to retrieve them.
+                if (!cells.get(0).equals("")) {
+                    // Obtain the corresponding MiniGame.
+                    try {
+                        miniGame = (Class<MiniGame>) Class.forName(className);
+                        Log2.write("Succesfully loaded class: \"" + miniGame.getName() + "\".", Log2.INFO);
+                        
+                        // The Exception catch here is only to catch of the shit-load of possible exceptions.
+                        // This is allowed here since there is no further error handeling then logging.
+                    } catch (Exception e) {
+                        Log2.write(new Object[] {
+                            ("Failed to load class: \"" + className + "\". Error:"), e
+                        }, Log2.ERROR);
+                    }
                     
-                    // The Exception catch here is only to catch of the shit-load of possible exceptions.
-                    // This is allowed here since there is no further error handeling then logging.
-                } catch (Exception e) {
-                    Log2.write(new Object[] {
-                        ("Failed to load class: \"" + className + "\". Error:"), e
-                    }, Log2.ERROR);
+                } else {
+                    Log2.write("Reference to class was empty!", Log2.WARNING);
                 }
                 
-                Word word = new Word(MultiTool.listToArray(cells, String.class, 1), langs, miniGame);
-                words.add(word);
-                Log2.write("Succesfully loaded word " + word + ".", Log2.INFO);
+                try {
+                    String imgFileName = (cells.get(0).equals("") ? "" : imgDir + cells.get(0) + ".png");
+                    Word word = new Word(MultiTool.listToArray(cells, String.class, 1),
+                                         langs, miniGame,
+                                         imgFileName);
+                    
+                    words.add(word);
+                    Log2.write("Succesfully loaded word " + word + ".", Log2.INFO);
+                    
+                } catch (IllegalArgumentException | NullPointerException e) {
+                    String errorText = "";
+                    for (int i = 0; i < langs.length; i++) {
+                        errorText += langs[i] + ": " + cells.get(i) + (i +1 != langs.length ? ", " : "");
+                    }
+                    
+                    Log2.write("Failed to create word: " + errorText, Log2.ERROR);
+                }
             }
             
         } catch (IOException e) {
             Log2.write(e);
         }
+        
+        Log2.write(new String[] {" === Finished creating word list === ", ""}, Log2.INFO);
         
         return words;
     }
