5a9cd922851abd27179d870ed5b9baf346156c71
Kaj0Wortel <36736012+Kaj0Wortel@users.noreply.github.com>
2018-03-08 22:40:20 +0100

    Changed to a more general setup

--- font/FontLoader.java
+++ font/FontLoader.java
@@ -11,7 +11,10 @@ package learningGame.font;
 
 // Own packages
 import learningGame.LearningGame;
+
 import learningGame.log.Log2;
+
+import learningGame.tools.MultiTool;
 
 
 // Java packages
@@ -19,22 +22,17 @@ import java.awt.Font;
 import java.awt.FontFormatException;
 import java.awt.GraphicsEnvironment;
 
+import java.io.File;
 import java.io.FileInputStream;
 import java.io.IOException;
+
+import java.util.ArrayList;
+import java.util.Hashtable;
 
 
 public class FontLoader {
     final private static String staticPath = LearningGame.workingDir + "font\\";
-    
-    final public static String COUSINE    = "Cousine";
-    final public static String COUSINE_I  = "Cousine-Italic";
-    final public static String COUSINE_B  = "Cousine-Bold";
-    final public static String COUSINE_BI = "Cousine-BoldItalic";
-    
-    final public static Font Cousine   = loadFont("cousine\\Cousine-Regular.ttf"   , Font.PLAIN);
-    final public static Font CousineI  = loadFont("cousine\\Cousine-Italic.ttf"    , Font.ITALIC);
-    final public static Font CousineB  = loadFont("cousine\\Cousine-Bold.ttf"      , Font.BOLD);
-    final public static Font CousineBI = loadFont("cousine\\Cousine-BoldItalic.ttf", Font.BOLD + Font.ITALIC);
+    final public static Hashtable<String, Font> fonts = new Hashtable<String, Font>();
     
     
     /* ----------------------------------------------------------------------------------------------------------------
@@ -47,34 +45,53 @@ public class FontLoader {
     @Deprecated
     private FontLoader() { }
     
+    
+    /* ----------------------------------------------------------------------------------------------------------------
+     * Functions
+     * ----------------------------------------------------------------------------------------------------------------
+     */
     /* 
      * Loads a font from a file.
      * 
-     * @param localPath the path of the font file relative to the font directoy
+     * @param localPath the path of the font file relative to the font directoy.
+     * @param path the path to the font file.
      * @param style the style of teh font. Should be one of: {@code Font.PLAIN},
      *     {@code Font.ITALIC}, {@code Font.BOLD} or {@code Font.BOLD + Font.ITALIC}
      */
-    public static Font loadFont(String localPath, int style) {
+    public static Font loadLocalFont(String localPath, int style) {
+        return loadFont(staticPath + localPath, style);
+    }
+    
+    public static Font loadFont(String path, int style) {
+        return loadFont(path, style, Font.TRUETYPE_FONT);
+    }
+    
+    public static Font loadFont(String path, int style, int type) {
         Font font = null;
         
-        try (FileInputStream fis = new FileInputStream(staticPath + localPath)) {
-            font = Font.createFont(Font.TRUETYPE_FONT, fis).deriveFont(style, 12F);
+        try (FileInputStream fis = new FileInputStream(path)) {
+            font = Font.createFont(type, fis).deriveFont(style, 12F);
+            fonts.put(path, font);
             
-        } catch (FontFormatException e) {
-            Log2.write(e);
-            
-        } catch (IOException e) {
+        } catch (FontFormatException | IOException e) {
             Log2.write(e);
         }
         
         return font;
     }
     
-    
-    /* ----------------------------------------------------------------------------------------------------------------
-     * Functions
-     * ----------------------------------------------------------------------------------------------------------------
+    /* 
+     * @param fontName the name of the font.
+     * @return the font.
      */
+    public static Font getLocalFont(String localName) {
+        return getFont(staticPath + localName);
+    }
+    
+    public static Font getFont(String fontName) {
+        return fonts.get(fontName);
+    }
+    
     /* 
      * Registers the given font.
      * When successfull, the font can be used in the application for e.g. html code.
@@ -82,7 +99,7 @@ public class FontLoader {
      * @param font the font to be registered
      */
     public static boolean registerFont(Font font) {
-        return GraphicsEnvironment.getLocalGraphicsEnvironment().registerFont(FontLoader.CousineB);
+        return GraphicsEnvironment.getLocalGraphicsEnvironment().registerFont(font);
     }
     
     
@@ -91,23 +108,51 @@ public class FontLoader {
      * ----------------------------------------------------------------------------------------------------------------
      */
     /* 
-     * Registers all fonts from this class.
+     * Registers all fonts in the fonts folder.
      * See registerFont(Font) for more info about registering a font.
      */
     static {
-        Font errorFont = null;
+        Log2.write(" === Start loading fonts === ", Log2.INFO);
+        ArrayList<File[]> files = MultiTool.listFilesAndPathsFromRootDir(new File(staticPath), false);
         
-        Font[] fonts = {
-            Cousine, CousineI, CousineB, CousineBI
-        };
-        
-        for (int i = 0; i < fonts.length; i++) {
-            if (fonts[i] == null) continue;
+        for (File[] file : files) {
+            String fontLoc = file[0].toString();
             
-            if (!registerFont(fonts[i])) {
-                Log2.write(new Object[] {("Could not register font: "), fonts[i]}, Log2.WARNING);
+            if (fontLoc.endsWith(".ttf")) {
+                int style = 
+                    (fontLoc.toLowerCase().contains("bold") ? Font.BOLD : 0) |
+                    (fontLoc.toLowerCase().contains("italic") ||
+                     fontLoc.contains("It") ? Font.ITALIC : 0);
+                if (style == 0) style = Font.PLAIN;
+                /*
+                System.out.println(style == Font.PLAIN ? "PLAIN"
+                                       : style == Font.BOLD ? "BOLD"
+                                       : style == Font.ITALIC ? "ITALIC"
+                                       : style == (Font.BOLD | Font.ITALIC) ? "BOLD + ITALIC"
+                                       : "ERROR!");
+                */
+                
+                Font font = loadFont(file[0].toString(), style, Font.TRUETYPE_FONT);
+                if (font == null) {
+                    Log2.write("A null font has been created: " + file[0].toString(), Log2.ERROR);
+                    
+                } else if (!registerFont(font)) {
+                    Log2.write("Could not register font: " + file[0].toString(), Log2.ERROR);
+                    
+                } else {
+                    Log2.write("Successfully loaded font: " + file[0].toString(), Log2.INFO);
+                }
+            } else {
+                Log2.write("Ignored file: " + file[0].toString(), Log2.INFO);
             }
+            
         }
+        
+        Log2.write(new String[] {" === Finished loading fonts === ", ""}, Log2.INFO);
+    }
+    
+    public static void main(String[] args) {
+        
     }
     
 }
